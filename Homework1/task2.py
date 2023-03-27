33d73578a39cabb08b8f249b35419ea98a3f7770
Andrey <Dr_Wo1f@mail.ru>
2023-03-27 22:02:46 +0300

    task2 finished

---
+++ Homework1/task2.py
@@ -0,0 +1,26 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+# *** Рассмотрите случай числа с плавающей точкой и не обязательно 3-х значного
+
+def Input():
+    while True:
+        inputCount = int(input("Введите трехзначное число: "))
+        if 99 < inputCount < 1000:
+            return inputCount
+
+def SumFounder(count):
+    summ = count % 10 + count % 100 // 10 + count // 100
+    return summ
+
+
+
+def Main():
+    count = Input()
+    print(count)
+    summ = SumFounder(count)
+    print(summ)
+
+
+Main()
