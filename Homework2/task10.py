36371b4fe3c62d328cd72a17e54287bc28a77466
Andrey <Dr_Wo1f@mail.ru>
2023-03-29 20:38:10 +0300

    task10 finished

--- Homework2/task10.py
+++ Homework2/task10.py
@@ -1,22 +1,36 @@
-# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, 
+# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой,
 # а некоторые – гербом. Определите минимальное число монеток, которые нужно
-# перевернуть, чтобы все монетки были повернуты вверх одной и той же 
-# стороной. Выведите минимальное количество монет, которые нужно 
+# перевернуть, чтобы все монетки были повернуты вверх одной и той же
+# стороной. Выведите минимальное количество монет, которые нужно
 # перевернуть
 
 def InputFunc():
-    count = int(input("Введите кол-во монеток"))
+    count = int(input("Введите кол-во монеток: "))
     monets = []
     for i in range(0, count):
-        monets.append(int(input("Введите 1 или 0: ")))
+        monets.append(int(input("Введите 1 (решка) или 0 (орел):  ")))
     print(monets)
     return monets
 
 
+def Calculation(tuple):
+    zeroCount = 0
+    oneCount = 0
+    for i in range(0, len(tuple)):
+        if tuple[i] == 0:
+            zeroCount += 1
+        else:
+            oneCount += 1
+    if zeroCount > oneCount:
+        result = oneCount
+    else:
+        result = zeroCount
+    print(f"Нужно перевернуть {result} монет(ы)")
+
+
 def Main():
     input = InputFunc()
-    #счетчик перевернутых монет...
-    pass
+    Calculation(input)
 
 
 Main()
